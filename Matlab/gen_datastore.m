d1332a26b9a47862c12df4985e2dc498be293b0a
Casper <caspervanderhorst@gmail.com>
2021-09-27 17:25:10 +0200

    generate data stores seperated in training, validation, and testing data stores

--- Matlab/gen_datastore.m
+++ Matlab/gen_datastore.m
@@ -20,11 +20,13 @@ sample_path = uigetdir(samples_root);
 if sample_path == 0
     return
 end
+
+label_to_split = "direction";
 %% Create general datastore
 
 audio_DS = audioDatastore(sample_path, 'FileExtensions', '.wav');
 
-%adding tlabel definitions
+%adding label definitions
 load ("label_definitions.mat")
 lss = labeledSignalSet;
 addLabelDefinitions(lss, labelDef);
@@ -55,4 +57,40 @@ total_partitions = training_partion + validation_partition + testing_partition;
 training_partion = training_partion/total_partitions;
 validation_partition = validation_partition/total_partitions;
 testing_partition = testing_partition/total_partitions;
+
+[training_DS, rem] = splitDualLabel(audio_DS, label_to_split, training_partion);
+[validation_DS, testing_DS] = splitDualLabel(rem, label_to_split, validation_partition/(1-training_partion));
+
+% plot distributions of the labels in data stores 
+% display_distribution(audio_DS, "all");
+% display_distribution(training_DS, "train");
+% display_distribution(validation_DS, "val");
+% display_distribution(testing_DS, "test");
+
+%% Save datastores
+
+d = split(sample_path,filesep);
+d(1:end-4) = [];
+
+metadata.top_state  = d{1};
+metadata.LoS_state  = d{2};
+metadata.sound_file = d{3};
+d = split(d{4},"_");
+metadata.sample_length = str2double(replace(d{end},"s","."));
+metadata.date = date;
+
+data.general_DS = audio_DS;
+data.training_DS = training_DS;
+data.validation_DS = validation_DS;
+data.testing_DS = testing_DS;
+data.metadata = metadata;
+
+
+file_name = "DS_" + data.metadata.date;
+if ~isfolder(fullfile(sample_path,"datastores")) 
+    mkdir(sample_path, "datastores")
+end
+save(fullfile(sample_path,"datastores",file_name),"data");
+
+
 
