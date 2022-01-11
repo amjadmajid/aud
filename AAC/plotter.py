8e8e1751d75f85eea2514eec2cd57f2bb6280950
Lucan de Groot <lucan.degroot@gmail.com>
2022-01-12 00:34:13 +0100

    This code is able to correctly plot a multiple transmitters.

--- AAC/plotter.py
+++ AAC/plotter.py
@@ -254,8 +254,9 @@ def plot_effective_bit_rate():
 
 
 def plot_subchirp_test():
-    df = pd.read_csv('./data/results/06-01-2022-dynamic-vs-fixed-sub-chirps/Recorded_files/parsed_results.csv')
-    # df = pd.read_csv('E:/Recorded_files/parsed_results.csv')
+    # df = pd.read_csv('./data/results/06-01-2022-dynamic-vs-fixed-sub-chirps/Recorded_files/parsed_results.csv')
+    df = pd.read_csv('E:/7-1-2022/parsed_results.csv')
+
     # df1 = pd.read_csv('E:/Recorded_files/parsed_results_single_transmitter.csv')
     # df1["transmitters"] = 1
     # df2 = pd.read_csv('E:/Recorded_files/parsed_results.csv')
@@ -266,21 +267,21 @@ def plot_subchirp_test():
     df = df.drop(columns="cycles")
 
     # conf, offset, Ts, transmittor_count, ber
-    df = df.groupby(["configuration", "Ts"]).ber.agg(["mean", "std"]).reset_index()
+    df = df.groupby(["configuration", "Ts", "transmitters"]).ber.agg(["mean", "std"]).reset_index()
 
     df = df.sort_values(by="Ts")
 
     plt.figure(figsize=(6, 3))
     for i, conf in enumerate(configurations):
-        # # for t in [1, 2, 3, 4]:
-        # data = df[(df.configuration == conf) &
-        #           (df.transmitters == t)]
-        data = df[(df.configuration == conf)]
+        for t in [1, 2, 3, 4]:
+            data = df[(df.configuration == conf) &
+                  (df.transmitters == t)]
+            # data = df[(df.configuration == conf)]
 
-        print(data)
+            print(data)
 
-        # plt.plot(data.Ts * 1000, data["mean"], label=conf + f"-{t}", marker=markers[i])
-        plt.plot(data.Ts * 1000, data["mean"], label=conf, marker=markers[i])
+            plt.plot(data.Ts * 1000, data["mean"], label=conf + f"-{t}", marker=markers[i])
+            # plt.plot(data.Ts * 1000, data["mean"], label=conf, marker=markers[i])
 
     # plt.hlines(0.01, 0, 25, linestyles='dotted', color='black')
     plt.ylim(-0.00001, 1.1)
@@ -294,14 +295,15 @@ def plot_subchirp_test():
 
 
 def scatter_multi_transmitter_range_test_results():
-    df1 = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results.csv')
+    # df1 = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results.csv')
     # df2 = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results_200cm.csv')
     # # df3 = pd.read_csv('./data/results/05-01-2022-multi-transmitter-reverberend/parsed_results_150.csv')
     # df4 = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results_100_150cm.csv')
     # df5 = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results_50cm.csv')
 
     # df = pd.concat([df1, df2, df4, df5])
-    df = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results.csv')
+    df = pd.read_csv('E:/Recorded_files/parsed_results.csv')
+    # df = pd.read_csv('./data/results/07-01-2022-multi-transmitter-los/parsed_results.csv')
     # df.to_csv("./all_data.csv", index=False)
 
     color_list = ["#7e1e9c", '#0343df', '#43a2ca', '#0868ac', '#eff3ff', '#0000ff']
