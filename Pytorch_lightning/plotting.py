0f7d0ac48bf256aec52ed7f6931df940969fef2c
Nils134 <61148684+Nils134@users.noreply.github.com>
2022-10-03 18:00:26 +0200

    Let plot file automatically select latest run version

--- Pytorch_lightning/plotting.py
+++ Pytorch_lightning/plotting.py
@@ -1,9 +1,21 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas
+import glob
+import os
+import ntpath
 
+
+version = 0
+#find path to latest version that has been logged into "./lightning_logs"
+for f in os.listdir('lightning_logs'):
+    print(f)
+    new_version = int(f[f.index('_')+1:])
+    print(new_version)
+    if new_version > version:
+        version = new_version
 #load up data collected data
-data = pandas.read_csv("lightning_logs/version_3/metrics.csv")
+data = pandas.read_csv("lightning_logs/version_"+ str(version) + "/metrics.csv")
 
 data.plot(x="epoch", y="val_loss")
 
